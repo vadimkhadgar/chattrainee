650ef44c1c7607c6a20c179cdd73b30e48aad8ee
Khadgar <blyashuk@list.ru>
2019-08-30 00:04:22 +0300

    Update 2.2

--- app/src/main/java/ru/vadimbliashuk/chattrainee/util/FirestoreUtil.kt
+++ app/src/main/java/ru/vadimbliashuk/chattrainee/util/FirestoreUtil.kt
@@ -3,21 +3,24 @@ package ru.vadimbliashuk.chattrainee.util
 import android.content.Context
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.QuerySnapshot
 import com.xwray.groupie.kotlinandroidextensions.Item
 import ru.vadimbliashuk.chattrainee.models.User
 import ru.vadimbliashuk.chattrainee.recyclerview.UserItem
 
 object FirestoreUtil {
-    private val firestoreInstance: FirebaseFirestore by lazy {FirebaseFirestore.getInstance()}
+    private val firestoreInstance: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
 
-   // private val currentUserDocRef: DocumentReference
+    // private val currentUserDocRef: DocumentReference
 
     fun addUserListener(context: Context, onListen: (List<Item>) -> Unit): ListenerRegistration {
         return firestoreInstance.collection("users")
-            .addSnapshotListener{ querySnapshot: QuerySnapshot?, firebaseFirestoreException: FirebaseFirestoreException? ->
+            .addSnapshotListener { querySnapshot: QuerySnapshot?, firebaseFirestoreException: FirebaseFirestoreException? ->
                 if (firebaseFirestoreException != null) {
-                    Log.e("FIRESTORE", "User Listener Error.",firebaseFirestoreException)
+                    Log.e("FIRESTORE", "User Listener Error.", firebaseFirestoreException)
                     return@addSnapshotListener
                 }
 
@@ -26,8 +29,9 @@ object FirestoreUtil {
                     if (it.id != FirebaseAuth.getInstance().currentUser?.uid) {
                         items.add(UserItem(it.toObject(User::class.java)!!, it.id, context))
                     }
-                    onListen(items)
                 }
+                onListen(items)
+
 
             }
     }
